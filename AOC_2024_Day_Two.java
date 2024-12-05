66985546537a2d6337129abe5c1a368a1da648ef
Shakib0000 <145377831+Shakib0000@users.noreply.github.com>
2024-12-05 14:51:18 -0500

    Update AOC_2024_Day_Two.java

--- AOC_2024_Day_Two.java
+++ AOC_2024_Day_Two.java
@@ -9,8 +9,10 @@ public class AOC_2024_Day_Two {
         ArrayList<String> fileData = getFileData("src/Input.txt");
 
         int safeReports = 0;
+        int problemDampenerSafeReports = 0;
         for (int i = 0; i < fileData.size(); i++) {
             boolean safe = true;
+            int problemCount = 0;
             String[] array = fileData.get(i).split(" ");
             String direction = "";
             if (Integer.parseInt(array[0]) < Integer.parseInt(array[1])) {
@@ -22,20 +24,30 @@ public class AOC_2024_Day_Two {
             for (int j = 0; j < array.length - 1; j++) {
                 if (direction.equals("decreasing") && ((Integer.parseInt(array[j]) - Integer.parseInt(array[j+1]) > 3) || Integer.parseInt(array[j]) - Integer.parseInt(array[j+1]) < 0)) {
                     safe = false;
+                    problemCount++;
                 }
                 if (direction.equals("increasing") && ((Integer.parseInt(array[j+1]) - Integer.parseInt(array[j]) > 3) || Integer.parseInt(array[j+1]) - Integer.parseInt(array[j]) < 0)) {
                     safe = false;
+                    problemCount++;
                 }
                 if ((Integer.parseInt(array[j]) == Integer.parseInt(array[j+1]))) {
                     safe = false;
+                    problemCount++;
                 }
             }
             if (safe) {
                 safeReports++;
+                problemDampenerSafeReports++;
+            }
+            else {
+                if (problemCount == 1) {
+                    problemDampenerSafeReports++;
+                }
             }
         }
 
         System.out.println("There are " + safeReports + " safe reports.");
+        System.out.println("Using the problem dampener, there are " + problemDampenerSafeReports + " safe reports.");
     }
 
     public static ArrayList<String> getFileData(String fileName) {
